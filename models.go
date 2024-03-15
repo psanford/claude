386635db629bfee80da399f82e07564a51c43eba
Peter Sanford <psanford@sanford.io>
2024-03-15 08:50:06 -0700

    Add Haiku

--- models.go
+++ models.go
@@ -3,6 +3,7 @@ package claude
 const (
 	Claude3Opus        = "claude-3-opus-20240229"
 	Claude3Sonnet      = "claude-3-sonnet-20240229"
+	Claude3Haiku       = "claude-3-haiku-20240307"
 	Claude2Dot1        = "claude-2.1"
 	Clause2Dot0        = "claude-2.0"
 	Claude1Dot2Instant = "claude-instant-1.2"
