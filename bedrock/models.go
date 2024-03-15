5b82ecb9421c26c4555e6585485714ccf47940e5
Peter Sanford <psanford@sanford.io>
2024-03-15 08:50:12 -0700

    Add consts for bedrock models
    These are specifically for bedrockruntime.InvokeModelInput.ModelId.

---
+++ bedrock/models.go
@@ -0,0 +1,13 @@
+package bedrock
+
+// BedrockModel is the field set in the bedrock request in InvokeModel
+// This is different than the model set in MessageRequest or TextCompletion
+type BedrockModel string
+
+const (
+	Claude3Sonnet      BedrockModel = "anthropic.claude-3-sonnet-20240229-v1:0"
+	Claude3Haiku       BedrockModel = "anthropic.claude-3-haiku-20240307-v1:0"
+	Claude2Dot1        BedrockModel = "anthropic.claude-v2:1"
+	Clause2Dot0        BedrockModel = "anthropic.claude-v2"
+	Claude1Dot2Instant BedrockModel = "anthropic.claude-instant-v1"
+)
